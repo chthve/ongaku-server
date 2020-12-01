c758d47cfeb8125bacb38c7f9ff6acaa546c27ca
Guillermo Schmithalter <gschmithalter14@gmail.com>
2020-12-01 18:35:57 +0100

    feat(mocks): create script to populate db automatically

---
+++ mocks/mock.js
@@ -0,0 +1,35 @@
+const users = [
+  {
+    username: 'Charles',
+  },
+  {
+    username: 'Gui',
+  },
+  {
+    username: 'Carlitos',
+  },
+];
+
+const channels = [
+  {
+    name: 'Electronic',
+  },
+  {
+    name: 'Jazz',
+  },
+  {
+    name: 'Rock',
+  },
+  {
+    name: 'Classical',
+  },
+];
+
+const tags = [
+  { name: 'chill' },
+  { name: 'after' },
+  { name: 'mushrooms' },
+  { name: 'sunset' },
+];
+
+module.exports = { users, channels, tags };
---
+++ mocks/populateDB.js
@@ -0,0 +1,14 @@
+const db = require('../models');
+const { users, tags, channels } = require('./mock');
+
+(async () => {
+  try {
+    await db.User.bulkCreate(users);
+    await db.Channel.bulkCreate(channels);
+    await db.Tag.bulkCreate(tags);
+
+    console.log('Data populated!!');
+  } catch (error) {
+    console.error('OOOPS, DATA WAS NOT POPULATED', error);
+  }
+})();
