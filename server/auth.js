03ce11383842e2cbed6a65aeeb355af6e2f7b77f
elijah <elijah_burke@hotmail.co.uk>
2020-12-02 17:17:56 +0000

    feat(.env file updated): environment variables updated with consumer key and token
    env files up to date

--- server/auth.js
+++ server/auth.js
@@ -9,15 +9,14 @@ passport.use(
       requestTokenURL: 'https://api.discogs.com/oauth/request_token',
       accessTokenURL: 'https://api.discogs.com/oauth/access_token',
       userAuthorizationURL: 'https://www.discogs.com/oauth/authorize',
-      consumerKey: 'hOqgGkAKtjmfpFYvNhjb',
-      consumerSecret: 'vgbWmkBoIOkDSxQOeuJFCqIMOBPSuiUf',
-      callbackURL: 'http://localhost:3001/auth/provider/callback',
+      consumerKey: process.env.consumerKey,
+      consumerSecret: process.env.consumerSecret,
+      callbackURL: process.env.callbackURL,
     },
     function (token, tokenSecret, profile, done) {
       db.User.findOrCreate({
         where: { token, tokenSecret },
       }).then((err, user) => {
-        console.log(err, user);
         done(err, user);
       });
     }
